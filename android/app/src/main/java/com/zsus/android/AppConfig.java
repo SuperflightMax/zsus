ab284d8aab96d52cf26be3d045b7d9ea8f9164e2
SuperflightMax <107130518+SuperflightMax@users.noreply.github.com>
2026-02-09 15:10:33 +0200

    apk passport update

--- android/app/src/main/java/com/zsus/android/AppConfig.java
+++ android/app/src/main/java/com/zsus/android/AppConfig.java
@@ -1,7 +1,9 @@
 package com.zsus.android;
 
 public final class AppConfig {
-    public static final String BASE_URL = "https://superflight.vps.webdock.cloud/zsus/";
+    //public static final String BASE_URL = "https://superflight.vps.webdock.cloud/zsus/bbs/";
+    //public static final String BASE_URL = "https://superflight.vps.webdock.cloud/zsus/uzhas/";
+    public static final String BASE_URL = "https://superflight.vps.webdock.cloud/zsus/uzhas/";
     public static final boolean KEEP_SCREEN_ON = true;
 
     private AppConfig() {
